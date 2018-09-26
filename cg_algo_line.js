7c18678dd31bcd40c82e55288f398724c082c870
Peng QiBiao <andreamapp@sina.com>
2018-09-26 22:09:57 +0800

    [Refactor] <style> to .css, <script> to .js

---
+++ cg_algo_line.js
@@ -0,0 +1,130 @@
+async function drawline_dda (x1, y1, x2, y2, color) {
+    let dm = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1)); // 迭代次数
+    let dx = (x2 - x1) / dm; // x步长
+    let dy = (y2 - y1) / dm; // y步长
+    let x = x1 + 0.5;
+    let y = y1 + 0.5;
+    function log(){
+        screen.log_list.push({
+            pix: {
+                x: Math.floor(x),
+                y: Math.floor(y),
+                color: color
+            },
+            log: 'x = ' + x.toFixed(2) + ', y = ' + y.toFixed(2) +
+            ', dx = ' + dx.toFixed(2) + ', dy = ' + dy.toFixed(2)
+        });
+    }
+    for(let i = 0; i <= dm; i++){
+        log();
+        await screen.drawpixel(Math.floor(x), Math.floor(y), color);
+        x += dx;
+        y += dy;
+    }
+}
+async function drawline_mid (x1, y1, x2, y2, color) {
+    let a, b, d, x, y, flag = 0;
+    function log(){
+        let _x = flag ? y : x;
+        let _y = flag ? x : y;
+        screen.log_list.push({
+            pix: {
+                x: _x,
+                y: _y,
+                color: color
+            },
+            log: 'x = ' + _x + ', y = ' + _y +
+            ', d = ' + d.toFixed(2)
+        });
+    }
+    if(Math.abs(x2 - x1) < Math.abs(y2 - y1)){
+        // 斜率大于1，交换坐标
+        [x1, y1] = [y1, x1];
+        [x2, y2] = [y2, x2];
+        flag = 1;
+    }
+    if(x1 > x2){
+        // 交换左右坐标
+        [x1, x2] = [x2, x1];
+        [y1, y2] = [y2, y1];
+    }
+    a = y1 - y2;
+    b = x2 - x1;
+    d = a + b / 2;
+    if(y1 < y2){
+        x = x1; y = y1;
+        log();
+        await screen.drawpixel(x, y, color, flag);
+        while(x < x2){
+            if(d < 0) {
+                x++; y++; d = d+a+b;
+            }
+            else{
+                x++; d += a;
+            }
+            log();
+            await screen.drawpixel(x, y, color, flag);
+        }
+    }
+    else{
+        x = x2; y = y2;
+        log();
+        await screen.drawpixel(x, y, color, flag);
+        while(x > x1){
+            if(d < 0){
+                x--; y++; d = d-a+b;
+            }
+            else{
+                x--; d -= a;
+            }
+
+            log();
+            await screen.drawpixel(x, y, color, flag);
+        }
+    }
+}
+async function drawline_bresenham (x1, y1, x2, y2, color) {
+    // await this.drawpixel(x1, y1, color);
+    let dx = Math.abs(x2 - x1), dy = Math.abs(y2 - y1);
+    let flag = 0;
+    // 将斜率变为 0 < |k| < 1
+    if(dx < dy){
+        flag = 1;
+        [x1, y1] = [y1, x1];
+        [x2, y2] = [y2, x2];
+        [dx, dy] = [dy, dx];
+    }
+    let tx = (x2 - x1) > 0 ? 1 : -1;
+    let ty = (y2 - y1) > 0 ? 1 : -1;
+    let curx = x1, cury = y1;
+    let dS = 2 * dy, dT = 2 * (dy - dx);
+    let d = dS - dx;
+
+    function log(){
+        let _x = flag ? cury : curx;
+        let _y = flag ? curx : cury;
+        screen.log_list.push({
+            pix: {
+                x: _x,
+                y: _y,
+                color: color
+            },
+            log: 'x = ' + _x + ', y = ' + _y +
+            ', d = ' + d.toFixed(2)
+        });
+    }
+    log();
+    await screen.drawpixel(curx, cury, color, flag);
+    while(curx !== x2) {
+        if(d < 0) {
+            d += dS;
+        }
+        else {
+            cury += ty;
+            d += dT;
+        }
+        curx += tx;
+        log();
+        await screen.drawpixel(curx, cury, color, flag);
+    }
+}
---
+++ screen.js
@@ -0,0 +1,168 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let screen = new Vue({
+    el: '#screen',
+    data: {
+        matrix: [],
+        screen_view: 'Grid',
+        width: 31,
+        delay: 50,
+        x1: 0,
+        y1: 0,
+        x2: 16,
+        y2: 18,
+        highlight_pix: {x: 0, y:0},
+        algorithm: 'DDA',
+        log_list: []
+    },
+    computed: {
+        view: {
+            get: function() {
+                return this.screen_view;
+            },
+            set: function(value) {
+                this.screen_view = value;
+
+            }
+        },
+        range: {
+            get: function() {
+                return Math.floor(this.width / 2);
+            },
+            set: function(value) {
+                if(value > 50) value = 50;
+                this.dim = value * 2 + 1;
+            }
+        },
+        dim: {
+            get: function() {
+                return this.width
+            },
+            set: function (length) {
+                this.width = length;
+                let matrix = [];
+                let L = Math.floor(length / 2);
+                for(let i = -L; i <= L; i++){
+                    let row = [];
+                    for(let j = -L; j <= L; j++){
+                        row.push({
+                            x: i,
+                            y: j,
+                            color: 0
+                        });
+                    }
+                    matrix.push(row);
+                }
+                this.matrix = matrix;
+                this.clear();
+            }
+        }
+    },
+    methods: {
+        __clear_grid: function () {
+            this.log_list = [];
+            let L = this.width;
+            let mid = Math.floor(L / 2);
+            for(let i = 0; i < L; i++) {
+                for (let j = 0; j < L; j++) {
+                    this.matrix[i][j] = {
+                        x: i - mid,
+                        y: mid - j,
+                        color: (i === mid || j === mid) ? -1 : 0
+                    }
+                }
+            }
+            this.$forceUpdate();
+        },
+        __clear_canvas: function() {
+            let canvas = document.getElementById("canvas");
+            if (canvas.getContext) {
+                let ctx = canvas.getContext("2d");
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                ctx.beginPath();
+                ctx.moveTo(0, 300);
+                ctx.lineTo(600, 300);
+                ctx.moveTo(300, 0);
+                ctx.lineTo(300, 600);
+                ctx.stroke();
+            }
+        },
+        clear: function () {
+            this.log_list = [];
+            if('Grid' === this.view){
+                this.__clear_grid();
+            }
+            else if('Canvas' === this.view){
+                this.__clear_canvas();
+            }
+        },
+        hoverPix: function(pix){
+            this.highlight_pix = pix;
+            for(let i = 0; i < this.log_list.length; i++){
+                this.log_list[i].highlight = this.highlight_pix.x === this.log_list[i].pix.x
+                    && this.highlight_pix.y === this.log_list[i].pix.y;
+                Vue.set(this.log_list, i, this.log_list[i]);
+            }
+        },
+        __drawpixel_grid: function(pix) {
+            let L = this.dim;
+            let x = Math.floor(pix.x + L / 2);
+            let y = L - Math.floor(pix.y + L / 2) - 1;
+            if(x >= 0 && y >= 0 && x < L && y < L){
+                Vue.set(this.matrix[x], y, pix);
+                return true;
+            }
+            return false;
+        },
+        __drawpixel_canvas: function(pix) {
+            let canvas = document.getElementById("canvas");
+            if (canvas.getContext) {
+                let ctx = canvas.getContext("2d");
+                let L = Math.floor(canvas.width / 2);
+                let x = pix.x + L;
+                let y = L - pix.y - 1;
+
+                ctx.fillStyle = 'red';
+                ctx.fillRect(x, y, 1, 1);
+                return true;
+            }
+            return false;
+        },
+        drawpixel: async function(x, y, color, reversed = false) {
+            let pix = {
+                x: reversed ? y : x,
+                y: reversed ? x : y,
+                color: color
+            };
+
+            let drawed = false;
+            if('Grid' === this.view){
+                drawed = this.__drawpixel_grid(pix);
+            }
+            else if('Canvas' === this.view){
+                drawed = this.__drawpixel_canvas(pix);
+            }
+            if(drawed && this.delay > 0){
+                await sleep(this.delay);
+            }
+        },
+
+        run: async function() {
+            this.clear();
+            if('DDA' === this.algorithm){
+                await drawline_dda(this.x1, this.y1, this.x2, this.y2, 1);
+            }
+            else if('MidPoint' === this.algorithm){
+                await drawline_mid(this.x1, this.y1, this.x2, this.y2, 1);
+            }
+            else if('Bresenham' === this.algorithm){
+                await drawline_bresenham(this.x1, this.y1, this.x2, this.y2, 1);
+            }
+        }
+    }
+});
+screen.range = 15;
