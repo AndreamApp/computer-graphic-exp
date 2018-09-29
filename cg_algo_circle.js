bc0f5b0e625cb1126b51da716bac76823fcb9d1c
Peng QiBiao <andreamapp@sina.com>
2018-09-29 16:21:15 +0800

    [Add] draw circle

---
+++ cg_algo_circle.js
@@ -0,0 +1,120 @@
+async function drawpixel_eight_way(x0, y0, x, y, color) {
+    await screen.drawpixel(x0 + x, y0 + y, color);
+    await screen.drawpixel(x0 + x, y0 - y, color);
+    await screen.drawpixel(x0 - x, y0 + y, color);
+    await screen.drawpixel(x0 - x, y0 - y, color);
+    await screen.drawpixel(x0 + y, y0 + x, color);
+    await screen.drawpixel(x0 + y, y0 - x, color);
+    await screen.drawpixel(x0 - y, y0 + x, color);
+    await screen.drawpixel(x0 - y, y0 - x, color);
+}
+
+async function drawpixel_four_way(x0, y0, x, y, color) {
+    await screen.drawpixel(x0 + x, y0 + y, color);
+    await screen.drawpixel(x0 + x, y0 - y, color);
+    await screen.drawpixel(x0 - x, y0 + y, color);
+    await screen.drawpixel(x0 - x, y0 - y, color);
+}
+
+async function draw_circle_mid(x0, y0, r, color) {
+    let x = 0, y = Math.floor(r);
+    let d = 1 - r;
+
+    function log(){
+        screen.log_list.push({
+            pix: {
+                x: x,
+                y: y,
+                color: color
+            },
+            log: 'x = ' + x + ', y = ' + y +
+            ', delta = ' + d
+        });
+    }
+    await screen.drawpixel(x0, y0, color);
+    while(x < y) {
+        log();
+        await drawpixel_eight_way(x0, y0, x, y, color);
+        if(d < 0){
+            d += 2 * x + 3;
+        }
+        else{
+            d += 2 * (x - y) + 5;
+            y--;
+        }
+        x++;
+    }
+    if(x === y) {
+        await drawpixel_eight_way(x0, y0, x, y, color);
+    }
+}
+
+async function draw_circle_bresenham(x0, y0, r, color) {
+    let x = 0, y = Math.floor(r);
+    let d = 2*(1 - r);
+    let HD, DV;
+
+    function log(){
+        screen.log_list.push({
+            pix: {
+                x: x,
+                y: y,
+                color: color
+            },
+            log: 'x = ' + x + ', y = ' + y +
+            ', delta = ' + d +
+            ', HD = ' + HD +
+            ', DV = ' + DV
+        });
+    }
+    await screen.drawpixel(x0, y0, color);
+    while(y > x) {
+        await drawpixel_eight_way(x0, y0, x, y, color);
+        HD = 2 * d + 2 * y - 1;
+        DV = 2 * d - 2 * x - 1;
+        log();
+        if(d < 0 && HD < 0) {
+            x++;
+            d = d + 2 * x + 1;
+        }
+        else if(d > 0 && DV < 0) {
+            y--;
+            d = d - 2 * y + 1;
+        }
+        else{
+            x++;
+            y--;
+            d = d + 2 * x - 2 * y + 2;
+        }
+    }
+    if(x === y) {
+        await drawpixel_eight_way(x0, y0, x, y, color);
+    }
+}
+
+async function draw_ellipse(x0, y0, a, b, color) {
+    let x = 0, y = b;
+    let d = 0;
+    while(a*a*y > b*b*x) {
+        await drawpixel_four_way(x0, y0, x, y, color);
+        d = b*b*(x+1)*(x+1) + a*a*(y-0.5)*(y-0.5) - a*a*b*b;
+        if(d < 0){
+            x++;
+        }
+        else{
+            x++;
+            y--;
+        }
+    }
+    while(y >= 0){
+        await drawpixel_four_way(x0, y0, x, y, color);
+        d = b*b*(x+0.5)*(x+0.5) + a*a*(y-1)*(y-1) - a*a*b*b;
+        if(d < 0){
+            x++;
+            y--;
+        }
+        else{
+            y--;
+        }
+    }
+}
--- screen.js
+++ screen.js
@@ -13,8 +13,16 @@ let screen = new Vue({
         y1: 0,
         x2: 16,
         y2: 18,
-        highlight_pix: {x: 0, y:0},
-        algorithm: 'DDA',
+        x0: 0,
+        y0: 0,
+        r: 10,
+        ellipse_x0: 0,
+        ellipse_y0: 0,
+        ellipse_a: 10,
+        ellipse_b: 6,
+        highlight_pix: {x: 0, y: 0},
+        algo_line: 'DDA',
+        algo_circle: 'MidPoint',
         log_list: []
     },
     computed: {
@@ -150,19 +158,32 @@ let screen = new Vue({
                 await sleep(this.delay);
             }
         },
-
-        run: async function() {
+        line: async function() {
             this.clear();
-            if('DDA' === this.algorithm){
+            if('DDA' === this.algo_line){
                 await drawline_dda(this.x1, this.y1, this.x2, this.y2, 1);
             }
-            else if('MidPoint' === this.algorithm){
+            else if('MidPoint' === this.algo_line){
                 await drawline_mid(this.x1, this.y1, this.x2, this.y2, 1);
             }
-            else if('Bresenham' === this.algorithm){
+            else if('Bresenham' === this.algo_line){
                 await drawline_bresenham(this.x1, this.y1, this.x2, this.y2, 1);
             }
-        }
+        },
+        circle: async function() {
+            this.clear();
+            if('MidPoint' === this.algo_circle){
+                await draw_circle_mid(this.x0, this.y0, this.r, 1);
+            }
+            else if('Bresenham' === this.algo_circle){
+                await draw_circle_bresenham(this.x0, this.y0, this.r, 1);
+            }
+        },
+        ellipse: async function() {
+            this.clear();
+            await draw_ellipse(this.ellipse_x0, this.ellipse_y0, this.ellipse_a, this.ellipse_b, 1);
+        },
+
     }
 });
 screen.range = 15;
